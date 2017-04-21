150e3b9980f593effcf94e0365bd4f5fc949e79f
Siltaar <siltaar@d12s.fr>
2017-04-21 18:21:51 +0200

    initial

---
+++ README.md
@@ -0,0 +1,37 @@
+# spam_test_rewrite
+Test the spam probability of an email based on idempotent rules (no learning).
+
+Expect email input via stdin and rewrite it to stdout adding an `X-Spam-Score` header.
+
+Created to be used with [FDM](https://github.com/nicm/fdm) in a `rewrite` action.
+
+## Rules and scores
+
+There are currently 2 sets of rules, each one is able to increment the spam score by 1.
+
+### Subject
+
+If the subject is missing, empty or contains less than half alphabetical letters, the spam score is incremented by 1.
+
+### Recipient count
+
+If there is no recipient or more than 9, the spam score is incremented by 1.
+
+## Performance note
+In a quest for performance, I compared Python 3 and Python 2 versions of the code. Python 2.7 appeared to be 20% to 30% quicker than Python 3.4 in Debian stable 8.7 for a sample of 80 randomly contact@ emails of a small french software company. So I kept the Python 2 version.
+
+The Python 3 version is also provided.
+
+Cython compilation was 50% slower in this edge case.
+
+## Example
+
+### `fdm.conf` example :
+  match all action rewrite "python2 -SE spam_test_rewrite2.py" continue
+  match "^X-Spam-Score: 1$" in headers action maildir "%h/.Maildir/.spam"
+  match "^X-Spam-Score: [2-9]$" in headers action maildir "%h/.Maildir/.furspam"
+
+## Tests
+
+To run the embeded testsuite :
+  python -m doctest spam_test_rewrite2.py
---
+++ spam_test_rewrite2.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python2
+# coding: utf-8
+# author : Simon Descarpentries, 2017-03
+# licence: GPLv3
+
+from __future__ import print_function
+from sys import stdin
+# from sys import stderr
+from email.parser import Parser
+from email.header import decode_header as decode_h
+from email.header import make_header as make_h
+from email.utils import getaddresses
+from curses.ascii import isalpha
+
+
+def spam_test(stdin_eml):
+	"""
+		# doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
+
+		>>> spam_test('To:a@a.tk\\nSubject: "Normal" email should pass')
+		... # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
+		From nobody ...
+		To: a@a.tk
+		Subject: "Normal" email should pass
+		X-Spam-Score: 0
+		<BLANKLINE>
+
+		>>> spam_test('To:\\nSubject: Missing recipient should be scored 1')
+		... # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
+		From nobody ...
+		To:
+		Subject: Missing recipient should be scored 1
+		X-Spam-Score: 1
+		<BLANKLINE>
+
+		>>> spam_test('To:a@a.tk, b@b.tk, c@c.tk, d@d.tk, e@e.tk, f@f.tk, g@g.tk, \
+				h@h.tk, i@i.tk, j@j.tk\\nSubject: More than 9 recipients, scored 1')
+		... # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
+		From nobody ...
+		To: a@a.tk, b@b.tk, c@c.tk, d@d.tk, e@e.tk, f@f.tk, g@g.tk, h@h.tk, i@i.tk,
+		 j@j.tk
+		Subject: More than 9 recipients, scored 1
+		X-Spam-Score: 1
+		<BLANKLINE>
+
+		>>> spam_test('To:a@a.tk\\nSubject: Not a half letters ..............................')
+		... # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
+		From nobody ...
+		To: a@a.tk
+		Subject: Not a half letters ..............................
+		X-Spam-Score: 1
+		<BLANKLINE>
+
+		>>> spam_test('To:a@a.tk\\n')
+		... # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
+		From nobody ...
+		To: a@a.tk
+		X-Spam-Score: 1
+		<BLANKLINE>
+
+		>>> spam_test('To:\\nSubject: 2 conditions scored 2 ..............................')
+		... # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
+		From nobody ...
+		To:
+		Subject: 2 conditions scored 2 ..............................
+		X-Spam-Score: 2
+		<BLANKLINE>
+	"""
+	eml = Parser().parsestr(stdin_eml, headersonly=True)  # Parse header of stdin piped email
+	score = 0
+
+	try:
+		refined_subject = unicode(make_h(decode_h(eml.get('Subject', '')))).encode('utf8')
+	except:  # Encoding exception ? Might be spam, and we can't continue
+		refined_subject = ''
+
+	alpha_length = len([c for c in refined_subject if isalpha(c)])
+
+	if alpha_length == 0 or len(refined_subject) / alpha_length > 2:
+		score += 1  # If not 1 ascii letter over 2 in the subject, I don't want to read it
+
+	recipient_count = len(getaddresses(eml.get_all('to', []) + eml.get_all('cc', [])))
+
+	if recipient_count > 9 or recipient_count < 1:
+		score += 1  # If there is no or more than 20 recipients, it may be a spam
+
+	eml['X-Spam-Score'] = str(score)
+	# print('%s score %i recipients %i' % (refined_subject, score, recipient_count), file=stderr)
+	# print(eml, file=stderr)
+	print(eml, end='')
+
+
+if __name__ == "__main__":
+	spam_test(stdin.read())
