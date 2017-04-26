75caa47df5a8da98931b47475324df09145ffed7
siltaar <siltaar@1761490.no-reply.drupal.org>
2017-04-26 15:23:53 +0200

    unify spam_test python version, and add 2 new rules : alpha From: name, already SPAM flagged ;

--- README.md
+++ README.md
@@ -7,35 +7,44 @@ Created to be used with [FDM](https://github.com/nicm/fdm) in a `pipe` action.
 
 ## Rules and scores
 
-There are currently 2 sets of rules, each one is able to increment the spam score by 1.
+There are currently 3 sets of rules, each one is able to increment the spam score by 1.
 
-### Subject
+### Subject and From
 
 If the subject is missing, empty or contains less than half ASCII characters, the spam score is incremented by 1.
+
+If the From name part is not empty but contains less than half ASCII characters, the spam score is incremented by 1.
 
 ### Recipient count
 
 If there is no recipients or more than 9, the spam score is incremented by 1.
 
+### X-Spam-Status
+
+If the messages has already been flagged as spam, the spam score is incremented by 1.
+
 ## Performance note
-In a quest for performance, I compared Python 3 and Python 2 versions of the code. Python 2.7 appeared to be 20% to 30% quicker than Python 3.4 in Debian stable 8.7 for a sample of 200 randomly contact@ emails of a small french software company. So I kept the Python 2 version.
+In a quest for performance, I compared Python 3 and Python 2 versions of the code. Python 2.7 appeared to be 20% to 30% quicker than Python 3.4 in Debian stable 8.7 for a sample of 200 randomly contact@ emails of a small french software company. So I kept using Python 2 and made a cross-compatible version.
 
-The Python 3 version is also provided.
+Cython compilation was 2 times slower in this edge case.
 
-Cython compilation was 50% slower in this edge case.
+`-O` and `-OO` python's options are slowing down the computation by 3.
 
 ## Example
 
 ### `spam_test` `fdm.conf` example :
 
 ```
-match pipe "python2 -SE spam_test2.py" returns (,'^([0-9]+)$') action tag "spam_score_%[command0]" continue
-match tagged "spam_score_1" action maildir "%h/.Maildir/.spam"
-match tagged "spam_score_2" action maildir "%h/.Maildir/.furspam"
+match pipe "python2 -SE spam_test.py" returns (,'^([0-9]+)$') action keep continue
+match string "%[command0]" to "1" action maildir "%h/.Maildir/.spam"
+match string "%[command0]" to "[2-9]" action maildir "%h/.Maildir/.furspam"
 ```
 
 
 ## Tests
 
 To run the embeded testsuite :
-`python2 -m doctest spam_test2.py`
+```shell
+python2 -m doctest spam_test.py
+python3 -m doctest spam_test.py
+```
---
+++ spam_test.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python2
+# coding: utf-8
+# author : Simon Descarpentries, 2017-03
+# licence: GPLv3
+
+from __future__ import print_function
+from sys import stdin, version_info
+# from sys import stderr
+from email.parser import Parser
+from email.header import decode_header
+from email.header import make_header
+from email.utils import getaddresses, parseaddr
+from curses.ascii import isalpha
+
+
+def spam_test(stdin_eml):
+	"""
+		>>> spam_test('From:Bb<b@b.tk>\\nTo:a@a.tk\\nSubject:"Normal" eml ok')
+		0
+		>>> spam_test('To:\\nSubject: Missing recipient should be scored 1')
+		1
+		>>> spam_test('Subject: No recp, 1 non-alpha =?utf-8?b?w6k=?= scored 1')
+		1
+		>>> spam_test('To:a@a.tk,b@b.tk,c@c.tk,d@d.tk,e@e.tk,f@f.tk,g@g.tk,'
+		...	'h@h.tk,i@i.tk,j@j.tk\\nSubject: More than 9 recipients, scored 1')
+		1
+		>>> spam_test('To:a@a.tk\\nSubject: Not half ASCII =?utf-8?b?w6nDqcOpw'
+		...	'6nDqcOpw6nDqcOpw6k=?=')
+		1
+		>>> spam_test('To:No subject scored 1 <a@a.tk>')
+		1
+		>>> spam_test('Subject: no To no ASCII scored 2=?utf-8?b?w6nDqcOpw6nD'
+		...	'qcOpw6nDqcOpw6nDqcOpw6nDqcOpw6nDqcOpw6nDqcOpw6k=?=')
+		2
+		>>> spam_test('Subject: =?gb2312?B?vNLT0NChxau499bW1sa3/sC009W78w==?=')
+		2
+		>>> spam_test('Subject: =?gb2312?B?Encoding error 代 ?=')
+		2
+		>>> spam_test('X-Spam-Status: Yes')
+		3
+		>>> spam_test('From: =?utf-8?b?5Luj?= <a@a.tk>')
+		3
+	"""
+	eml = Parser().parsestr(stdin_eml, headersonly=True)  # Parse header of stdin piped email
+	score = 0
+
+	if eml.get('X-Spam-Status', '').lower() == 'yes' or \
+			eml.get('X-Spam-Flag', '').lower() == 'yes':
+		score += 1  # if already flagged as spam, we should get cautious
+
+	subj_len, subj_alpha_len = header_alpha_length(eml.get('Subject', ''))
+
+	if subj_alpha_len == 0 or subj_len // subj_alpha_len > 1:
+		score += 1  # If no more than 1 ascii char over 2 in subject, I can't read it
+
+	recipient_count = len(getaddresses(eml.get_all('to', []) + eml.get_all('cc', [])))
+
+	if recipient_count == 0 or recipient_count > 9:
+		score += 1  # If there is no or more than 9 recipients, it may be a spam
+
+	from_len, from_alpha_len = header_alpha_length(parseaddr(eml.get('From', ''))[0])
+
+	if from_len > 0 and (from_alpha_len == 0 or from_len // from_alpha_len > 1):
+		score += 1  # If no more than 1 ascii char over 2 in from name, I can't read it
+
+	# print('score %i alpha %i To: %i alpha_To %i' % (
+	#	score, subj_alpha_len, recipient_count, from_alpha_len), file=stderr)
+	print(str(score), end='')
+
+
+def header_alpha_length(h):
+	try:
+		refined_h = unicode(make_header(decode_header(h)))
+	except:
+		refined_h = ''
+
+	# print(refined_h, end=' ', file=stderr)
+	h_len = len(refined_h)
+	ascii_h = refined_h.encode('ascii', 'ignore')
+	h_alpha_len = len([c for c in ascii_h if isalpha(c)])
+	return h_len, h_alpha_len
+
+
+try: type(unicode)  # If unicode is missing we're in Python 3, should fix this
+except: globals()['unicode'] = lambda s: str(s)
+
+if __name__ == "__main__":
+	if version_info.major > 2:
+		from io import TextIOWrapper
+		spam_test(TextIOWrapper(stdin.buffer, errors='ignore').read())
+	else:
+		spam_test(stdin.read())
